532e9fcdd255b33026445eaf92cd8a701d66cf47
luyuan <1101873740@qq.com>
2018-10-15 09:44:27 +0800

    [update] application 抽离

--- src/main/java/com/zfy/component/basic/arch/BaseApplication.java
+++ src/main/java/com/zfy/component/basic/arch/BaseApplication.java
@@ -2,6 +2,7 @@ package com.zfy.component.basic.arch;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.march.common.Common;
 import com.zfy.component.basic.ComponentX;
@@ -17,12 +18,11 @@ public abstract class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // ComponentX.init(this, true);
+        ComponentX.init(this, true);
         Common.init(this, getBuildConfigClazz());
     }
 
     public abstract Class getBuildConfigClazz();
-
 
     @Override
     protected void attachBaseContext(Context base) {
