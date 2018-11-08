45a6c0c0915fb4e1eadc6fe2edff8e5a1b0fb42d
luyuan <1101873740@qq.com>
2018-11-08 22:23:40 +0800

    [update] test main

--- app/src/main/java/com/zfy/light/sample/MainActivity.java
+++ app/src/main/java/com/zfy/light/sample/MainActivity.java
@@ -1,7 +1,18 @@
 package com.zfy.light.sample;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.march.common.exts.ListX;
+import com.zfy.adapter.LightAdapter;
+import com.zfy.adapter.LightHolder;
+import com.zfy.adapter.decoration.LinearDividerDecoration;
 import com.zfy.component.basic.mvx.mvp.app.MvpActivity;
 import com.zfy.component.basic.mvx.mvp.app.MvpV;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * CreateAt : 2018/11/8
@@ -12,8 +23,20 @@ import com.zfy.component.basic.mvx.mvp.app.MvpV;
 @MvpV(layout = R.layout.main_activity)
 public class MainActivity extends MvpActivity {
 
+
+    @BindView(R.id.content_rv) RecyclerView mRecyclerView;
+
+
     @Override
     public void init() {
-
+        List<String> strings = ListX.range(100, String::valueOf);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.addItemDecoration(new LinearDividerDecoration(getContext(), LinearDividerDecoration.VERTICAL, R.drawable.comm_divider);
+        mRecyclerView.setAdapter(new LightAdapter<String>(getContext(), strings, R.layout.item_simple) {
+            @Override
+            public void onBindView(LightHolder holder, String data, int pos) {
+                holder.setText(R.id.sample_tv, data);
+            }
+        });
     }
 }
